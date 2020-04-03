3bde8d869545ab0c75e708447c45ad90efe2ad35
andreiia <62594725+andreiia@users.noreply.github.com>
2020-04-03 13:25:55 +0300

    Update enumerari.cs

--- Lab5/enumerari.cs
+++ Lab5/enumerari.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +12,14 @@ namespace proiect
         Romantism = 3,
         Specialitate = 4,
         Fictiune = 5,
+    };
+
+    public enum STATUT
+    {
+        Elev = 1,
+        Student = 2,
+        Angajat = 3,
+        Pensionar = 4,
     };
 
 
